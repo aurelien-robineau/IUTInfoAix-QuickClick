f3fc6d770218bfaddfca07bbf29c7c8ddd60e939
Aurélien Robineau <aurelien.robineau@etu.univ-amu.fr>
2020-05-19 11:52:11 +0200

    Change db password

--- database/database.php
+++ database/database.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $dbh = new PDO('mysql:host=mysql-aure-rob.alwaysdata.net;dbname=aure-rob_bd', 'aure-rob', 'unmdp');
+    $dbh = new PDO('mysql:host=mysql-aure-rob.alwaysdata.net;dbname=aure-rob_bd', 'aure-rob', 'Robineau123.');
 } catch (Exception $e) {
     $_SESSION['error'] = $e->getCode() . " : " . $e->getMessage();
-    header('Location: index.php');
+    header('Location: ../index.php');
 }
